9d35796cb9c812c943b1587336ed3e05170e5b1c
Adonis Lee Villamor <adonisv79@gmail.com>
2022-12-21 07:45:08 +0000

    used apollo-server 4

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,31 +1,56 @@
-var express = require('express');
-var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
 
-const SERVER_PORT = 4000;
-const SERVER_PATH = '/graphql';
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello: String
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
   }
-`);
 
-// The root provides a resolver function for each API endpoint
-var root = {
-  hello: () => {
-    return 'Hello world!';
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+// Resolvers define how to fetch the types defined in your schema.
+// This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
   },
 };
 
-export default function startServer() {
-    var app = express();
-    app.use(SERVER_PATH, graphqlHTTP({
-      schema: schema,
-      rootValue: root,
-      graphiql: true,
-    }));
-    app.listen(SERVER_PORT);
-    console.log(`Running a GraphQL API server at http://localhost:${SERVER_PORT}${SERVER_PATH}`);
-}
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4001 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
